4e45afe6bd39764c11dfea32d4ee71d61399ee46
Joe <joe.joeb29@gmail.com>
2021-11-13 17:36:44 +0000

    LocalProvider fixed

--- config/src/main/java/joe/barker/config/Config.kt
+++ config/src/main/java/joe/barker/config/Config.kt
@@ -3,6 +3,7 @@ package joe.barker.config
 import android.content.Context
 import joe.barker.data.repository.MovieDetailsRepositoryImpl
 import joe.barker.domain.MovieDetailsUseCaseImpl
+import joe.barker.local.LocalProvider
 import joe.barker.remote.RemoteProvider
 
 class Config(context: Context) {
