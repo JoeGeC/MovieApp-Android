4d668d19586798d96b4b61718527af31eb8634da
Joe <joe.joeb29@gmail.com>
2021-11-15 19:56:21 +0000

    Tested successful movie details remote call

--- remote/src/main/java/joe/barker/remote/movieDetails/MovieDetailsRemoteImpl.kt
+++ remote/src/main/java/joe/barker/remote/movieDetails/MovieDetailsRemoteImpl.kt
@@ -7,8 +7,11 @@ import joe.barker.repository.boundary.MovieDetailsRemote
 import joe.barker.repository.response.ErrorResponse
 import joe.barker.repository.response.MovieDetailsResponse
 
-class MovieDetailsRemoteImpl : BaseRemote(), MovieDetailsRemote {
-    private val remote: MovieDetailsRemoteCalls = retrofit.create(MovieDetailsRemoteCalls::class.java)
+class MovieDetailsRemoteImpl(
+    private val remote: MovieDetailsRemoteCalls = retrofit.create(
+        MovieDetailsRemoteCalls::class.java
+    )
+) : BaseRemote(), MovieDetailsRemote {
 
     override fun getMovieDetails(movieId: Long): Result<MovieDetailsResponse?, ErrorResponse?> {
         val result = remote.retrieveMovie(movieId, API_KEY).execute()
--- remote/src/test/kotlin/joe/barker/remote/MovieDetailsRemoteShould.kt
+++ remote/src/test/kotlin/joe/barker/remote/MovieDetailsRemoteShould.kt
@@ -1,4 +1,28 @@
 package joe.barker.remote
 
+import joe.barker.remote.movieDetails.MovieDetailsRemoteCalls
+import joe.barker.remote.movieDetails.MovieDetailsRemoteImpl
+import joe.barker.repository.response.MovieDetailsResponse
+import joe.barker.repository.response.Result
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+import retrofit2.mock.Calls
+
 class MovieDetailsRemoteShould {
+    private val apiKey = BaseRemote.API_KEY
+
+    @Test
+    fun `Return movie details response on successful call`(){
+        val id: Long = 1
+        val responseBody = MovieDetailsResponse(id, "title", "tagline", "overview", "2020-01-02")
+        val remoteCalls = mock<MovieDetailsRemoteCalls> {
+            on { retrieveMovie(id, apiKey) }.doReturn(Calls.response(responseBody))
+        }
+        val repository = MovieDetailsRemoteImpl(remoteCalls)
+
+        val expected = Result.Success(responseBody)
+        assertEquals(expected, repository.getMovieDetails(id))
+    }
 }
