f83be4bdb03128470d4acfc938a7c53826d56376
Joe <joe.joeb29@gmail.com>
2021-11-26 00:31:30 +0000

    Started on Popular movie list view

--- app/src/main/java/joe/barker/movieapp/MainActivity.kt
+++ app/src/main/java/joe/barker/movieapp/MainActivity.kt
@@ -3,35 +3,32 @@ package joe.barker.movieapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import coil.compose.rememberImagePainter
-import joe.barker.domain.entity.MovieDetails
 import joe.barker.movieapp.page.MovieDetailsPage
-import joe.barker.movieapp.ui.ErrorUi
-import joe.barker.movieapp.ui.LoadingUi
-import joe.barker.movieapp.ui.MovieDetailsUi
+import joe.barker.movieapp.popularMovies.PopularMovieModel
 import joe.barker.movieapp.ui.theme.MovieAppTheme
-import kotlinx.coroutines.*
-import kotlin.coroutines.CoroutineContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -62,19 +59,75 @@ fun NavigationComponent(navController: NavHostController) {
     }
 }
 
-@Preview
 @Composable
 fun PopularPage() {
-    val movieUrls = listOf("https://image.tmdb.org/t/p/w500/rjkmN1dniUHVYAtwuV3Tji7FsDO.jpg")
+    val movies = listOf(
+        PopularMovieModel(
+            580489,
+            "Venom: Let There Be Carnage",
+            "15 Oct 2021",
+            7.2f,
+            "https://image.tmdb.org/t/p/w500/rjkmN1dniUHVYAtwuV3Tji7FsDO.jpg"
+        )
+    )
     LazyRow{
-        items(movieUrls){ movieUrl ->
-            Modifier.padding(16.dp)
-            Image(
-                painter = rememberImagePainter(movieUrl),
-                contentDescription = "Movie poster",
-                modifier = Modifier.width(128.dp)
-            )
+        items(movies){ movie ->
+            Column(modifier = Modifier.width(160.dp)) {
+                Box{
+                    Image(
+                        painter = rememberImagePainter(movie.posterUrl),
+                        contentDescription = "Movie poster",
+                        modifier = Modifier
+                            .width(160.dp)
+                            .clip(RoundedCornerShape(10.dp)),
 
+                    )
+                    RatingCircle(movie.voteAverage)
+                }
+                Text(
+                    text = movie.title,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(horizontal = 12.dp)
+                )
+                Text(
+                    text = movie.releaseDate,
+                    color = Color.Gray,
+                    modifier = Modifier.padding(horizontal = 12.dp)
+                )
+            }
         }
     }
+}
+
+@Composable
+private fun RatingCircle(rating: Float) {
+    Box(contentAlignment= Alignment.Center,
+        modifier = Modifier
+            .size(48.dp)
+            .background(Color.Black, shape = CircleShape)){
+        CircularProgressIndicator(
+            progress = rating / 10,
+            color = percentageCircleColor(rating),
+        )
+        Text(
+            text = "${rating.asPercentage()}%",
+            color = Color.White,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(4.dp)
+        )
+    }
+}
+
+fun Float.asPercentage(): String = (this * 10).toInt().toString()
+
+fun percentageCircleColor(percentage: Float): Color {
+    if(percentage < 4) return Color.Red
+    if(percentage < 7) return Color.Yellow
+    return Color.Green
+}
+
+@Preview
+@Composable
+private fun RatingCircle() {
+    RatingCircle(7.0f)
 }
---
+++ app/src/main/java/joe/barker/movieapp/popularMovies/PopularMovieModel.kt
@@ -0,0 +1,11 @@
+package joe.barker.movieapp.popularMovies
+
+class PopularMovieModel(
+    val id: Long,
+    val title: String,
+    val releaseDate: String,
+    val voteAverage: Float,
+    val posterUrl: String
+) {
+
+}
